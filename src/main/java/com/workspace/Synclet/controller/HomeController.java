c53ce5b2b648028e486a2293ecaaf38aa827dc72
Nikita Pavlyk <nikita.pavlyk.official@gmail.com>
2025-01-02 07:43:11 +0100

    task: Added full-worked header and footer for website.

--- src/main/java/com/workspace/Synclet/controller/HomeController.java
+++ src/main/java/com/workspace/Synclet/controller/HomeController.java
@@ -1,4 +1,17 @@
 package com.workspace.Synclet.controller;
 
+
+import com.workspace.Synclet.config.SecurityConfig;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }

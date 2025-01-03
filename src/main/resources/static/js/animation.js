eedfc17ce9aad7da5ff9b390fa84873e3707a9ad
Nikita Pavlyk <nikita.pavlyk.official@gmail.com>
2025-01-03 03:35:13 +0100

    task: Added section home.

---
+++ src/main/resources/static/js/animation.js
@@ -0,0 +1,17 @@
+
+const elements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+elements.forEach(element => {
+    observer.observe(element);
+});
